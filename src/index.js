f75bc966aedcd136246836afd7289c4bc4496c25
Josh Fry <joshdfry@me.com>
2019-08-27 10:40:01 -0700

    Update index.js
    Change `componentWillMount` to `UNSAFE_componentWillMount`

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ export default function withSideEffect(
         return !shallowEqual(nextProps, this.props);
       }
 
-      componentWillMount() {
+      UNSAFE_componentWillMount() {
         mountedInstances.push(this);
         emitChange();
       }
